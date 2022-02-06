fa2b7c9b4cc483034cd7e9aa0144df093bde7f16
5-Fifths <47290088+5-Fifths@users.noreply.github.com>
2022-02-06 00:20:10 -0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+std::string lowercase(std::string input) {
+	for (int i = 0; i < input.length(); i++)
+		if (input[i] >= 'A' && input[i] <= 'Z')
+			input[i] += 32;
+
+	return input;
+}
+
+int main() {
+	std::string string;
+
+	std::cin >> string;
+	std::cin.sync();
+	string = lowercase(string);
+
+	return 0;
+}
